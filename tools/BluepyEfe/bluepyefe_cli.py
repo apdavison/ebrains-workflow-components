9c04c01a0ec7aa4dc5a3262e3e221ae78912947e
Cristiano Köhler <c.koehler@fz-juelich.de>
2024-04-22 15:29:04 +0200

    Main function with notebook example

--- tools/BluepyEfe/bluepyefe_cli.py
+++ tools/BluepyEfe/bluepyefe_cli.py
@@ -6,6 +6,11 @@ by finding crossing of a set phase-value in the channel signals.
 import argparse
 from pathlib import Path
 
+from bluepyefe.cell import Cell
+
+
+# CLI parsing
+
 CLI = argparse.ArgumentParser()
 CLI.add_argument("--input_file_current", nargs='?', type=Path, required=True,
                  help="path with current measurement of the recording")
@@ -33,11 +38,33 @@ CLI.add_argument("--ljp", nargs='?', type=float,
 CLI.add_argument("--protocol_name", nargs='?', type=str, required=True,
                  help='name of the experimental protocol')
 
+
 def extract_features(input_file_current, input_file_voltage,
                      output_file, features, current_unit,
                      voltage_unit, time_unit, time_step,
                      ljp, protocol_name):
-    pass
+
+    # Create Cell component to load the recordings
+    cell = Cell(name="Default Cell")
+
+    # Metadata dictionary to load the recordings
+    files_metadata = {
+        "i_file": input_file_current,
+        "v_file": input_file_voltage,
+        "i_unit": current_unit,
+        "v_unit": voltage_unit,
+        "t_unit": time_unit,
+        "dt": time_step,
+        "ljp": ljp,
+    }
+
+    cell.read_recordings(protocol_data=[files_metadata],
+                         protocol_name=protocol_name)
+
+    cell.extract_efeatures(protocol_name=protocol_name,
+                           efeatures=features)
+
+    print(cell.recordings[0].efeatures)
 
 
 if __name__ == '__main__':
