a56db16b75fa3ebe7cf067b12476823a1fcd77d7
Moritz-Alexander-Kern <92092328+Moritz-Alexander-Kern@users.noreply.github.com>
2024-07-19 17:37:30 +0200

    first

--- tools/elephant/image/utils.py
+++ tools/elephant/image/utils.py
@@ -1,7 +1,7 @@
 import quantities as pq
 import neo
 import datetime
-
+from nixio.exceptions.exceptions import InvalidFile
 
 def select_data(data, block_idx=0, segment_idx=0, spike_train_idx=None, analog_signal_idx=None):
     if spike_train_idx is not None:
@@ -12,20 +12,39 @@ def select_data(data, block_idx=0, segment_idx=0, spike_train_idx=None, analog_s
         raise ValueError("spike_train or analog_signal index not provided")
 
 
-def load_data(input_file, input_format=None):
-    if not input_format:
+def load_data(input_file, input_format=None, block_index=None, block_name=None):
+    if input_format is None:
         candidate_io = neo.list_candidate_ios(input_file)
         if candidate_io:
             io_class = candidate_io[0]
             flags = ["ro"] if io_class.__qualname__ == "NixIO" else []
             io = io_class(input_file, *flags)
         else:
-            raise ValueError(f"Please specify the input format, provided: {input_format}")
+            raise ValueError(f"Please specify a valid input format, provided: {input_format}")
     else:
         flags = ["ro"] if input_format == "NixIO" else []
-        io = getattr(neo.io, input_format)(input_file, *flags)
-
-    return io.read()
+        try:
+            io = getattr(neo.io, input_format)(input_file, *flags)
+        except AttributeError:
+            raise ValueError(f"Input_format is not supported by neo, provided: {input_format}")
+        except InvalidFile:
+            raise ValueError("input_file and input_format do not match, please provide valid file and correct input format")
+    if block_index and block_name:
+        raise ValueError("Can not load by name and index simultaneously")
+    elif block_index is not None:
+        try:
+            return io.read()[block_index]
+        except TypeError:
+            raise ValueError("input_file and input_format do not match, please provide valid file and correct input format")
+    elif block_name is not None:
+        try:
+            block = next((block for block in io.read() if block.name == block_name), None)
+            if not block:
+                raise ValueError("No block with block_name found")
+            else: 
+                return block
+        except TypeError:
+            raise ValueError("input_file and input_format do not match, please provide valid file and correct input format")
 
 
 def save_data(data, output_file, output_format, action):
