3f9fc66217b469879071508b30b3e43f0efaf6aa
fbonnier <florent.bonnier@cnrs.fr>
2024-08-05 11:35:39 +0200

    Add xorg and freesurfer deps

--- tools/fmriprep/image/py-fmriprep/package.py
+++ tools/fmriprep/image/py-fmriprep/package.py
@@ -3,44 +3,32 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-fmriprep
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-fmriprep
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
-from spack import *
+from spack.package import *
 
 
-class Fmriprep(AutotoolsPackage):
-    """FIXME: Put a proper description of your package here."""
+class PyFmriprep(PythonPackage):
+    # Project description
+    """fMRIPrep is a functional magnetic resonance imaging (fMRI) data preprocessing pipeline that is designed to provide an easily accessible, state-of-the-art interface that is robust to variations in scan acquisition protocols and that requires minimal user input, while providing easily interpretable and comprehensive error and output reporting."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://pypi.org/project/fmriprep/"
-    url      = "https://github.com/LLNL/mpileaks/releases/download/v1.0/mpileaks-1.0.tar.gz"
+    # FMRIPrep home page.
+    homepage = "https://fmriprep.org/en/stable/"
+    url      = "https://github.com/nipreps/fmriprep/archive/refs/tags/24.0.0.tar.gz"
+    pypi     = "fmriprep/fmriprep-24.0.0.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
+    # FIXME: Add a list of maintainers
+    # maintainers = ['']
 
-    version('24.0.0', sha256='')
+    # FMRIPrep last release
+    version('24.0.0', sha256='387a9b16f721aafc9e4ae77fb3328741b844afd9dec4297fabec6f0faa5ea89e')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
-
-    def configure_args(self):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    # Dependencies.
+    depends_on('python@3.8:', type=('build', 'run'))
+    depends_on('py-nipype', type=('build', 'run'))
+    depends_on('fsl', type=('build', 'run')) # FSL (version 6.0.7.7)
+    depends_on('ants@2.5.1', type=('build', 'run')) # ANTs (version 2.5.1)
+    # depends_on('afni', type=('build', 'run')) # AFNI (version 24.0.05)
+    depends_on('c3d', type=('build', 'run')) # C3D (version 1.4.0)
+    depends_on('freesurfer@7.3.2', type=('build', 'run')) # FreeSurfer (version 7.3.2)
+    depends_on('py-bids-validator', type=('build', 'run')) # bids-validator (version 1.14.0)
+    # depends_on('connectome-workbench', type=('build', 'run')) # connectome-workbench (version 1.5.0)
