b035b4a3f12a693dec92f78a125ea61072a5c88c
Cristiano Köhler <c.koehler@fz-juelich.de>
2024-11-21 10:25:06 +0100

    Added function to save the wavelet transform to pickle files and to produce visualization plots. Removed unused CLI parameters

--- tools/elephant/image/wavelet_transform_cli.py
+++ tools/elephant/image/wavelet_transform_cli.py
@@ -2,36 +2,70 @@
 
 import argparse
 from pathlib import Path
+from collections import Iterable
 
+import numpy as np
+import matplotlib.pyplot as plt
 import elephant
-from utils import load_data, save_data, prepare_data, select_data, quantity_arg
+from utils import load_data, select_data
 
 CLI = argparse.ArgumentParser()
 CLI.add_argument("--input_file", nargs="?", type=Path, required=True, help="path with file with the input data")
 CLI.add_argument("--input_format", nargs="?", type=str, default=None, help="format of the input data")
 CLI.add_argument("--output_file", nargs="?", type=Path, required=True, help="path to the file where to write data")
-CLI.add_argument("--output_format", nargs="?", type=str, required=True, help="format of the output data")
 CLI.add_argument("--block_index", nargs="?", type=int, default=0, help="Index of the block to process (default: 0)")
 CLI.add_argument("--block_name", nargs="?", type=str, default=None, help="Name of the block to process (optional)")
 CLI.add_argument("--segment_index", nargs="?", type=int, default=0, help="Index of the segment to process (default: 0)")
 CLI.add_argument("--analog_signal_index", nargs="?", type=int, default=0, help="Index of the analog signal to process (default: 0)")
-CLI.add_argument("--action", nargs="?", type=str, required=True, help="Action on how to store the results with respect to the original data")
+CLI.add_argument("--visualization_plots", type=bool, default=True, help="Generate visualization plots for each input signal. It is averaged over channels.")
 CLI.add_argument("--frequency", nargs="?", type=float, required=True, help="Center frequency of the Morlet wavelet in Hz")
 CLI.add_argument("--n_cycles", nargs="?", type=float, default=6.0, help="Size of the mother wavelet (default: 6.0)")
 CLI.add_argument("--sampling_frequency", nargs="?", type=float, default=1.0, help="Sampling rate of the input data in Hz (default: 1.0)")
 CLI.add_argument("--zero_padding", nargs="?", type=bool, default=True, help="Specifies whether the data length is extended by padding zeros (default: True)")
 
 
+def _plot_wavelet_transform(output_path,
+                            input_signal,
+                            wavelet_transform_signal,
+                            signal_index,
+                            frequency):
+    if not isinstance(frequency, Iterable):
+        frequency = [frequency]
+    frequency = np.atleast_1d(np.array(frequency).squeeze())
+
+    wavelet_spectrum = np.abs(np.atleast_3d(wavelet_transform_signal))
+    avg_wavelet_spectrum = np.nanmean(wavelet_spectrum, axis=1)
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    im = ax.pcolor(input_signal.times,
+                   frequency,
+                   np.transpose(avg_wavelet_spectrum),
+                   cmap='cividis', shading='auto')
+    fig.colorbar(im)
+
+    time_units = input_signal.times.units.dimensionality
+    ax.set_ylabel("Frequency [Hz]")
+    ax.set_xlabel(f"Time [{time_units}]")
+    ax.set_title(f"Wavelet Spectrum - Signal {signal_index}")
+
+    fig.savefig(output_path / f"wavelet_spectrum_{signal_index}.pdf",
+                format="pdf")
+
+def _save_wavelet_transform(transformed_signals, output_file):
+    arrays = {i: array for i, array in enumerate(transformed_signals)}
+    np.savez(arrays, output_file)
+
+
 def wavelet_transform(
     input_file,
     input_format,
     output_file,
-    output_format,
     block_index,
     block_name,
     segment_index,
     analog_signal_index,
-    action,
+    visualization_plots,
     frequency,
     n_cycles,
     sampling_frequency,
@@ -63,17 +97,14 @@ def wavelet_transform(
         for signal in signals
     ]
 
-    # Save data to pickle
-    # save_transform()
+    # Plot visualization of the transformed signals
+    if visualization_plots:
+        output_path = output_file.parent
+        for index, signal in enumerate(transformed_signals):
+            _plot_wavelet_transform(output_path, signal, index, frequency)
 
-    def save_transform():
-        # TODO: implement
-        pass
-
-    def plot_transform():
-        # TODO: implement
-        pass
-        
+    # Save the wavelet coefficients to a pickle file
+    _save_wavelet_transform(transformed_signals, output_file)
         
 if __name__ == "__main__":
     args, unknown = CLI.parse_known_args()
